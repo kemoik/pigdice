2ad118f407877489fd62af8930e1b99e739e2804
caleb <ck.caleb17@gmail.com>
2019-03-02 10:03:59 +0300

    add roll ad hold button to the page

---
+++ js/sccripts.js
@@ -0,0 +1,78 @@
+//BUSINESS LOGIC
+var player = function(name,score,status){
+  this.name = name;
+  this.score = score;
+  this.turn = turn;
+}
+var player1 =[];
+var player2 =[];
+
+player.prototype.addScore = function(){
+  this.score += finalScore;
+}
+
+var dice = 0;
+var finalScore=0;
+ function roll() {
+   var player1 =[];
+   var player2 =[];
+  var dice = Math.floor(Math.random()*6)+ 1;
+  if(dice != 1){
+    (finalScore += dice)
+  }else {
+    (finalScore = 0);
+    change();
+  }
+}
+function start(){
+  var fName=document.getElementById("one").value;
+  var sName=document.getElementById("two").value;
+  document.getElementById("onee").innerHTML=fName;
+  document.getElementById("twoo").innerHTML=sName;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//UI LOGIC
+$(document).ready(function(){
+  $(".entry").submit(function(event){
+    event.preventDefault();
+    player1 = $("input#one").val();
+    player2 = $("input#two").val();
+    $("#onee").text(player1);
+    $("#twoo").text(player2);
+    $("div#game").toggle();
+    $(".entry").hide();
+});
+
+    $("#hold").click(function() {
+      if(player1.turn === "enabled") {
+        player1.addScore();
+        $("score-one").text(player1.score);
+        winner();
+        change();
+      } else if (player2.status == "enabled"){
+        player2.addScore();
+        $("score-two").text(player2.score);
+        winner();
+        change();
+     }
+        finalScore = 0
+        $("#scores").text(finalscore)
+});
+        $("#roll").click(function() {
+          roll();
+          $("#scores").text(finalscore)
+        })
+
+
+});
